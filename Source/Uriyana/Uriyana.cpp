7312f587ef726f5d09c9d6e4f71875486cc1a46f
Kunal SAgar <kunalsagar1999@gmail.com>
2020-07-08 13:44:50 +0530

    init

---
+++ Source/Uriyana/Uriyana.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Uriyana.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Uriyana, "Uriyana" );
